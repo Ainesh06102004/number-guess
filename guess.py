3ae804d25aa15c9e24e97a2b49b791b35bf30719
Ainesh Nanda <67788476+Ainesh06102004@users.noreply.github.com>
2021-07-05 19:06:40 +0530

    Add files via upload

--- guess.py
+++ guess.py
@@ -1,17 +1,26 @@
 import random
 
+print("Guessing Game ")
+print("Guess A Number Between 1 To 9")
 
-compnumber = random.randint(0,9)
+chances = 0
 
-print("Guess a number between 0 and 9, Good luck :)")
+number = random.randint(1, 9)
 
-usernumber = int(input("Enter your number: "))
+while chances < 5:
+    guess = int(input("Enter The Guessed Number : "))
 
-if(usernumber > 9 or usernumber < 0):
-    print("Enter a number between 0 and 9")
+    if guess == number:
+        print("Congratulation You Won The Game :)")
+        break
 
-if(usernumber >= 0 and usernumber <= 9):
-    if (compnumber == usernumber):
-        print("Congratulations! You've Won")    
+    elif guess < number:
+        print("Your Guess Was Too Low. Guess A Number Higher Than", guess)
+
     else:
-        print("Oops! Try again")
+        print("Your Guess Was Too High. Guess A Number Lower Than", guess)
+
+    chances += 1
+
+    if not chances < 5:
+        print("You Lose !!! The Number Is", number, "Try Again :)")
